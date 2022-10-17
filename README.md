b55ffeca3826fa13ba9145f864a043559a9202b2
Lucas Zluhan <lszluhan@hotmail.com>
2022-10-17 14:29:40 -0300

    marketing integration into container and minor formatting fixes

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 Monolithic Dashboard APP. Created to practice microfrontend architecture pattern.
 
-- 1 host app as 'container'
-- 3 remote apps as 'marketing (home page, pricing page), auth (sign in page, sign up page) and dashboard (dashboard page)'
+-   1 host app as 'container'
+-   3 remote apps as 'marketing (home page, pricing page), auth (sign in page, sign up page) and dashboard (dashboard page)'
 
-- React: container, marketing and auth
-- Vue: dashboard
+-   React: container, marketing and auth
+-   Vue: dashboard
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import App from './App'
 
 const mount = (el) => {
-    ReactDOM.render(<App/>, el);
-};
-
-if (process.env.NODE_ENV === 'development') {
-    const devRoot = document.querySelector('#_marketing-dev-root');
-
-    devRoot ? mount(devRoot) : null;
+    ReactDOM.render(<App />, el)
 }
 
-export { mount };
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector('#_marketing-dev-root')
+
+    devRoot ? mount(devRoot) : null
+}
+
+export { mount }
